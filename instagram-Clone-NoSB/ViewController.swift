5fe81709b7ad8fb5dc0cd5ee73508e5e68859c62
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-09-06 13:15:04 +0300

    Initial Commit

---
+++ instagram-Clone-NoSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  instagram-Clone-NoSB
+//
+//  Created by Samet Korkmaz on 6.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
